411a550f20c1ca9fdf60c8397c526a3268b4a4c1
keios-cn <keios.cn@hotmail.com>
2014-07-02 22:05:24 +0800

    Update digest_common.hxx
    Update 1

--- src/digest_common.hxx
+++ src/digest_common.hxx
@@ -7,9 +7,36 @@ class DigestInterface
 public:
     virtual void CTX_Initialize() = 0;
     virtual void CTX_Update(const void* p, size_t len) = 0;
-    virtual void CTX_Final() = 0;
-    virtual size_t DigestLength() = 0;
+    virtual void CTX_Finish() = 0;
+    virtual size_t GetDigestLength() = 0;
     virtual bool GetDigest(void* p, size_t& len) = 0;
+    virtual const char* GetDigestName() = 0;
+};
+
+
+class DigestFactory
+{
+public:
+    virtual DigestInterface* CreateDigester() = 0;
+    virtual const char* GetDigestName() = 0;
+};
+
+
+class DigestCommon : public DigestInterface
+{
+public:
+    DigestCommon() : m_initilized(false), m_finished(false)
+    {}
+
+    virtual ~DigestCommon()
+    {}
+
+
+
+protected:
+    bool m_initilized;
+    bool m_finished;
+
 };
 
 
