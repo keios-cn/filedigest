a2e6477a025520ff420bbc3c2c9e310945e9a539
keios-cn <keios.cn@hotmail.com>
2014-07-02 21:15:58 +0800

    Update digest_common.hxx

--- src/digest_common.hxx
+++ src/digest_common.hxx
@@ -6,10 +6,10 @@ class DigestInterface
 {
 public:
     virtual void CTX_Initialize() = 0;
-    virtual void CTX_Update(void* p, size_t len) = 0;
+    virtual void CTX_Update(const void* p, size_t len) = 0;
     virtual void CTX_Final() = 0;
     virtual size_t DigestLength() = 0;
-    virtual bool GetDigest(void* p, size_t len) = 0;
+    virtual bool GetDigest(void* p, size_t& len) = 0;
 };
 
 
