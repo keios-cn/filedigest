ff14b61e2c02cf957006296eabba1867f245ba4e
keios-cn <play@keios-vista.(none)>
2014-07-11 22:24:18 +0800

    Retab...

--- src/md4.hxx
+++ src/md4.hxx
@@ -24,35 +24,35 @@ public:
     static const char* const name;
 
 private:
-	MD4_Digester()
-	{
-		Initialize();
-	}
+    MD4_Digester()
+    {
+        Initialize();
+    }
 
-	MD4_CTX m_ctx;
-	unsigned char m_result[16];
+    MD4_CTX m_ctx;
+    unsigned char m_result[16];
 };
 
 
 class MD4_Factory: public DigesterFactory
 {
-	friend class MD4_Digester;
+    friend class MD4_Digester;
 public:
     virtual Digester* CreateDigester()
     {
-    	return MD4_Digester::CreateDigester();
+        return MD4_Digester::CreateDigester();
     }
     virtual const char* GetDigestName()
     {
-    	return MD4_Digester::name;
+        return MD4_Digester::name;
     }
 
 protected:
-	MD4_Factory()
-	{}
+    MD4_Factory()
+    {}
 
 private:
-	static MD4_Factory instance;
+    static MD4_Factory instance;
 };
 
 
